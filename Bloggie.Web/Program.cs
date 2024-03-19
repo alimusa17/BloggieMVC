ba57a03739775321223822bfbb37dad4f0647377
Ali Musa <alimusa_@abv.bg>
2024-03-19 14:05:44 +0200

    Changed HTTP request pipeline

--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
